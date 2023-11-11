9cf4f4f5a34e42ead3970510095c43f84ec4f3e9
Amarnath K <amarnathkunnummal112@gmail.com>
2023-11-11 18:43:13 +0530

    ur mom strikes again

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ st.set_page_config(page_title='Student Analysis', page_icon = 'https://images.ap
 with open('./config.yaml') as file:
     CONFIG = yaml.load(file, Loader=SafeLoader)
 
-st.beta_set_page_config(page_title='your_title', page_icon = 'https://images.app.goo.gl/46ngdkqwdCEaQoWb9', layout = 'wide', initial_sidebar_state = 'auto')
 st.title('Silver Hills Public School')
 
 student_col, teacher_col = st.columns(2)

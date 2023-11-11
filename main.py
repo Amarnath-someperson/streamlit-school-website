053f003c68bef54bbb9b676c6ec85d2c1a7294cb
Amarnath K <amarnathkunnummal112@gmail.com>
2023-11-11 18:13:10 +0530

    change favicon and title

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import numpy as np
 import yaml
 from yaml.loader import SafeLoader
 
+st.beta_set_page_config(page_title='Student Analysis', page_icon = favicon)
+
 with open('./config.yaml') as file:
     CONFIG = yaml.load(file, Loader=SafeLoader)
 
+st.beta_set_page_config(page_title='your_title', page_icon = 'https://images.app.goo.gl/46ngdkqwdCEaQoWb9', layout = 'wide', initial_sidebar_state = 'auto')
 st.title('Silver Hills Public School')
 
 student_col, teacher_col = st.columns(2)

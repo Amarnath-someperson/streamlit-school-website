3139c620417e230dab3039166c874da0df9fc3c6
Amarnath K <amarnathkunnummal112@gmail.com>
2023-11-14 18:42:56 +0530

    fix small shit

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import streamlit as st
 from authentication.authenticator import Authenticate
 from st_pages import Page, show_pages, add_page_title
 
+
 st.set_page_config(page_title='Student Analysis', page_icon ='favicon.png')
+show_pages(
+    [
+        Page("main.py", "Home", "🏠"),
+        Page("pages/admin.py", "Site Administration", ":warning:"),
+    ]
+)
 
 hide_st_style = """
     <style>
@@ -13,12 +20,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.sidebar.title('Constructing...')
 st.sidebar.write('something will be here soon...')
-show_pages(
-    [
-        Page("main.py", "Home", "🏠"),
-        Page("pages/admin.py", "Site Administration", ":warning:"),
-    ]
-)
+
 
 
 st.title('Silver Hills Public School')
